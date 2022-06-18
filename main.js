f45263371ac8ac77d5e52459821e1413956c2700
sanjoli2009 <106424394+sanjoli2009@users.noreply.github.com>
2022-06-18 12:17:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){}
+function setup(){
+canvas = createCanvas(280,280);
+canvas.center();
+background("white");
+
+function clearCanvas(){
+    background("white");
+}
+}
+function draw(){
+
+}
